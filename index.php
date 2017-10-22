9c7e1b7bafe28b6788bbac171f7fdf40bd76afb1
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-10-22 17:50:16 +0000

    route updated

--- index.php
+++ index.php
@@ -1,22 +1,32 @@
 <?php
 
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
+include dirname(__FILE__) . '/vendor/altorouter/altorouter/AltoRouter.php';
 
-switch ($request_uri[0]) {
-    case '/':
-        require 'views/home.php';
-        break;
-    case '/org':
-        require 'views/organization.php';
-        break;
-    case '/cat-add':
-        require 'views/category-add.php';
-        break;
-    case '/dashboard':
-        require 'views/dashboard.php';
-        break;
-    default:
-        
-        require 'views/404.php';
-        break;
+$router = new AltoRouter();
+
+$router->setBasePath('https://techstar-dashboard-tortuvshin.c9users.io');
+
+// map homepage
+$router->map( 'GET', '/', function() {
+	require __DIR__ . '/views/home.php';
+});
+
+$router->map( 'GET', '/home', function() {
+	require __DIR__ . '/views/home.php';
+});
+$router->map( 'GET', '/admin', function() {
+	require __DIR__ . '/views/dashboard.php';
+});
+$router->map( 'GET', '/cat-add', function() {
+	require __DIR__ . '/views/category-add.php';
+});
+
+$match = $router->match();
+
+if( $match && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	require 'views/404.php';
 }
+?>
